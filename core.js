8d5dbc7c8ba4f82ae6e85808374f2ef0779f1484
Anton <antonkanashov@mail.ru>
2019-05-26 21:32:28 +0300

    v0.1.1

--- core.js
+++ core.js
@@ -1,65 +1,62 @@
-const CORE_DT = 0.1; //delta time 100 ms
+const CORE_DT = 0.3; //delta time 300 ms
 const CORE_SCREEN_W = 128;
 const CORE_SCREEN_H = 128;
+const CORE_V = 'v0.1.1';
 
-let visibleObjects = [];
+let procs = ['MAIN'];
+let g = null;
+let currentProc = 'MAIN';
+
+//SETTINGS
+//setTime(new Date('2019-05-26T20:52').getTime()/1000);
 
 
-var CoreLogo = {
 
-    timer: 0,
 
-    init: () => {
-        this.timer = 0;
-    },
 
-    render: (g) => {
-        if(this.timer < 3){
-            g.setColor(1,1,1);
-            g.fillRect(0, 0, CORE_SCREEN_W, CORE_SCREEN_H);
 
-            g.setColor(0.8, 0.1, 1);
-            g.setFontVector(32);
-            g.drawString('DOS', CORE_SCREEN_W/2 - 40, CORE_SCREEN_H/2 - 16);
 
-            for(var i=0; i<this.timer*2; i++){
-                g.drawCircle(CORE_SCREEN_W/2 - i*8, CORE_SCREEN_H - 10, i*1+2);
-                g.drawCircle(CORE_SCREEN_W/2 + i*8, CORE_SCREEN_H - 10, i*1+2);
-            }          
 
-        }
-    },
 
-    update: () => {
-        if(this.timer < 3){
-            this.timer += CORE_DT;
+
+
+
+
+
+
+
+
+function mainScreen() {    
+    let menu = ['Files', 'Settings', 'Programs'];
+    let selected = 0;
+
+    if(currentProc === 'MAIN') {
+        g.setColor(1, 1, 1);
+        g.fillRect(0, 0, CORE_SCREEN_W, CORE_SCREEN_H);
+        g.setColor(0.7, 0, 1);
+        g.drawString(new Date(Date.now())
+            .toISOString().replace('T', ' ').substring(0, 16), 3, 2);
+        g.drawLine(0, 10, CORE_SCREEN_W, 10);
+        g.drawString('DOS ' + CORE_V, CORE_SCREEN_W-42, CORE_SCREEN_H-10);
+
+        for(var i=0; i<menu.length; i++) {
+            if(i == selected){
+                g.setColor(0.7, 0, 1);
+                g.fillRect(0, 10*i+10, CORE_SCREEN_W, 10*i+20);
+                g.setColor(1,1,1);                
+            } else {
+                g.setColor(0.7, 0, 1);                
+            }
+            g.drawString(menu[i], 4, 10*i+12);
+            g.drawLine(0, 10*i+20, CORE_SCREEN_W, 10*i+20);
         }
     }
-};
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-visibleObjects.push(CoreLogo);
 
 function initCore() {
 
-    visibleObjects.forEach(el => {
-        el.init();
-    });
+    
 
     //B15.set();// VCC
     //B14.reset(); // GND
@@ -67,37 +64,26 @@ function initCore() {
 
     var spi = new SPI();
     spi.setup({mosi:B15 /* sda */, sck:B13 /* scl */});
-    var g = require("ILI9163").connect(spi, A1 /* DC */, A3 /* CE */, A2 /* RST */, function() {
+    g = require("ILI9163").connect(spi, A1 /* DC */, A3 /* CE */, A2 /* RST */, function() {
         g.clear();
-        g.setRotation(2);
-        g.setColor(0.8, 0.5, 0.9);
+        g.setRotation(1);
+        g.setColor(0, 0, 0);
         g.fillRect(0, 0, 128, 128);
-        g.setColor(1, 0, 0);
-        g.drawString("Hello",0,0);
-        g.setFontVector(16);
-        g.setColor(0,0.5,1);
-        g.drawString("Espruino",0,10);
+        g.setColor(0.5, 0.5, 0.5);
+        g.drawString(CORE_V,0,0);        
 
-        setInterval(() => {
-            coreUpdate();
-            coreRender(g);
-        }, CORE_DT*1000);
+        currentProc = 'MAIN';
+        mainScreen();
     });
 }
 
 function coreUpdate() {
-    visibleObjects.forEach(el => {
+    procs.forEach(el => {
         el.update();
     });
 }
 
-function coreRender(g) {
-    g.clear();
 
-    visibleObjects.forEach(el => {
-        el.render(g);
-    });
-}
 
 
 
